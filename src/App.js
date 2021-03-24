ddd78ca7d19bebf67215f160cc07f9fb6e3e70af
Paula Krupa <paula.krupa@gmail.com>
2021-03-24 22:34:54 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <svg>
-            <rect className="blink"/>
-        </svg>
-    </div>
-  );
+    return (
+        <div className="App">
+            <svg viewBox="-15 -15 130 130" className="svg">
+                <defs>
+                    <filter id="shadow">
+                        <feDropShadow
+                            dx="0"
+                            dy="0"
+                            stdDeviation="5"
+                            floodColor="#000000"
+                            floodOpacity="3"
+                        />
+                    </filter>
+                </defs>
+                <rect className="shapeSize shapeColor" filter="url(#shadow)"/>
+                {/*<rect className="shapeSize shapeColor"/>*/}
+            </svg>
+        </div>
+    );
 }
 
 export default App;
